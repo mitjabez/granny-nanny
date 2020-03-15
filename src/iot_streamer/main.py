54cf1a9ce6525c6f5b06940fecd080645a55f62c
Mitja Bezensek <bezensek.mitja@gmail.com>
2020-03-15 07:31:34 +0100

    Write server time.

--- src/iot_streamer/main.py
+++ src/iot_streamer/main.py
@@ -1,37 +1,40 @@
 from google.cloud import monitoring_v3
 
-import time
 import base64
 import json
+import dateutil.parser as dp
 
 PROJECT = '***REMOVED***'
 
-def write_metric(device_id: str, registry_id: str, light: bool):
+
+def timestamp_to_epoch(timestamp: str):
+    return int(dp.parse(timestamp).strftime('%s'))
+
+
+def write_metric(device_id: str, registry_id: str, light: bool, timestamp_seconds: int):
     client = monitoring_v3.MetricServiceClient()
     project_name = client.project_path(PROJECT)
 
     series = monitoring_v3.types.TimeSeries()
     series.metric.type = 'custom.googleapis.com/light'
     series.resource.type = 'global'
-    series.metric.labels['deviceId'] = device_id
-    series.metric.labels['registryId'] = registry_id
+    series.metric.labels['device_id'] = device_id
+    series.metric.labels['registry_id'] = registry_id
     point = series.points.add()
     point.value.int64_value = 1 if light else 0
-    now = time.time()
-    point.interval.end_time.seconds = int(now)
+    point.interval.end_time.seconds = int(timestamp_seconds)
     point.interval.end_time.nanos = int(
-        (now - point.interval.end_time.seconds) * 10**9)
+        (timestamp_seconds - point.interval.end_time.seconds) * 10**9)
     client.create_time_series(project_name, [series])
-    print('Successfully wrote time series.')
-
 
 
 def iot_streamer(event, context):
     device_id = event['attributes']['deviceId']
     registry_id = event['attributes']['deviceRegistryId']
     payload = base64.b64decode(event['data']).decode('utf-8')
-    print(f'Got payload: {payload}');
+    print(f'Writing: {payload} at {context.timestamp}');
     decoded_payload = json.loads(payload)
     light = decoded_payload['light']
-    write_metric(device_id=device_id, registry_id=registry_id, light=light)
+    timestamp_seconds = timestamp_to_epoch(context.timestamp)
+    write_metric(device_id=device_id, registry_id=registry_id, light=light, timestamp_seconds=timestamp_seconds)
 
